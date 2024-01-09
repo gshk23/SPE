26ab0689c432b988cb6ddaf645c1888ffa6716b2
gshk23 <Sai.Hemanth@iiitb.ac.in>
2024-01-09 11:43:07 +0530

    commited by team2

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,8 @@
-//this is for team1
+//this is for team2
 import java.util.*;
 class helloworld{
    public static void main(String [] args){
      System.out.print("hdhhdhd");
-     System.out.print("program by team1");
+     System.out.print("program by team2");
    }
 }
