8d76465d86b944e547d15409e9b6b193d1bed337
gshk23 <155605953+gshk23@users.noreply.github.com>
2024-01-25 12:01:53 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class helloworld{
    public static void main(String [] args){
-     System.out.print("hdhhdhd");
-     System.out.print("program by team2");
+    System.out.print("Seyfer is reconciling, yahoo");
    }
 }
