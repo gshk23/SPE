93c9924e7e906a8bdad6404e3188c22f27c785b6
gshk23 <155605953+gshk23@users.noreply.github.com>
2024-01-25 11:59:03 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-//this is  masters
-=======
-//this is bty team1
->>>>>>> team1
-=======
-//this is by team2
->>>>>>> team2
 import java.util.*;
 class helloworld{
    public static void main(String [] args){
      System.out.print("hdhhdhd");
-<<<<<<< HEAD
-=======
      System.out.print("program by team2");
->>>>>>> team1
    }
 }
